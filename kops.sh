59c2d06330c5ae01c05a8e93f011d976a530715d
Mani19909 <127381119+Mani19909@users.noreply.github.com>
2025-05-07 15:37:28 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -14,5 +14,5 @@ mv kops-linux-amd64 /usr/local/bin/kops
 aws s3api create-bucket --bucket sirisha.k8s.local --region us-east-1
 aws s3api put-bucket-versioning --bucket sirisha.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
 export KOPS_STATE_STORE=s3://sirisha.k8s.local
-kops create cluster --name mani_dev.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
-kops update cluster --name mani_dev.k8s.local --yes --admin
+kops create cluster --name mani.dev.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
+kops update cluster --name mani.dev.k8s.local --yes --admin
